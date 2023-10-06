3914100e222c7a10e6452d61209b518eb706b0f0
Andre Davidian <davidianaa@masters.edu>
2023-10-06 23:03:35 +0000

    added bootstrap card to Employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ApplicationRecord
-  has_one_attached :profile_picture
+  has_one_attached :profile_picture do |attachable| 
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
