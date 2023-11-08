e48167866881da1ac451107ac415cd43d6870cc6
Andre Davidian <davidianaa@masters.edu>
2023-11-08 04:57:15 +0000

    added links to search results;

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,4 +2,8 @@ class Device < ApplicationRecord
   belongs_to :manufacturer, optional: true       
   belongs_to :category, optional: true       
   belongs_to :employee, optional: true  
+
+  def full_device_name
+    name + " " + "(" + manufacturer.name + "/" + category.name + ")"
+  end
 end
